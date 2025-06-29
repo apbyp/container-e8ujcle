9396afbd00261d102d485034a209dd76b9132f73
apbyp <ayushprobroyoking@gmail.com>
2025-06-29 14:12:55 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template
-from db import get_db, close_db
-import sqlalchemy
-from sqlalchemy import text
-from logger import log
 
 app = Flask(__name__)
-app.teardown_appcontext(close_db)
 
 
 @app.route("/")
@@ -13,21 +8,4 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/health")
-def health():
-    log.info("Checking /health")
-    db = get_db()
-    health = "BAD"
-    try:
-        result = db.execute(text("SELECT NOW()"))
-        result = result.one()
-        health = "OK"
-        log.info(f"/health reported OK including database connection: {result}")
-    except sqlalchemy.exc.OperationalError as e:
-        msg = f"sqlalchemy.exc.OperationalError: {e}"
-        log.error(msg)
-    except Exception as e:
-        msg = f"Error performing healthcheck: {e}"
-        log.error(msg)
-
-    return health
+app.run(debug=True, host="0.0.0.0")
